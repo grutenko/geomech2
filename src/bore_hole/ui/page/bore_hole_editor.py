89411b2a789d7c1cd1f5ca06932184236b102991
Alexey <alexfedor932@gmail.com>
2025-03-16 00:46:06 +0300

    misc

--- src/bore_hole/ui/page/bore_hole_editor.py
+++ src/bore_hole/ui/page/bore_hole_editor.py
@@ -9,6 +9,16 @@ class BoreHoleEditor(wx.Panel):
         self.is_new = is_new
         self.o = o
         self.parent_object = parent_object
+        sz = wx.BoxSizer(wx.VERTICAL)
+        self.splitter = wx.SplitterWindow(self)
+        self.left = wx.Panel(self.splitter)
+        l_sz = wx.BoxSizer(wx.VERTICAL)
+        self.left.SetSizer(l_sz)
+        self.right = wx.Notebook(self.splitter)
+        self.splitter.SplitVertically(self.left, self.right)
+        sz.Add(self.splitter, 1, wx.EXPAND)
+        self.SetSizer(sz)
+        self.Layout()
 
     def get_name(self):
         if self.is_new:
